709f748f954be68f4f3830c808f44f3ef0ef2f43
Gabriel Custodio <gabrieldsc24@gmail.com>
2025-09-18 23:07:34 -0300

    chore: renomeando id do mod

--- src/main/java/net/gabecust/tutorialmod/ExampleMod.java
+++ src/main/java/net/gabecust/tutorialmod/ExampleMod.java
@@ -1,6 +1,9 @@
-package com.example.examplemod;
+package net.gabecust.tutorialmod;
+
+import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.food.FoodProperties;
@@ -27,7 +30,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MODID)
