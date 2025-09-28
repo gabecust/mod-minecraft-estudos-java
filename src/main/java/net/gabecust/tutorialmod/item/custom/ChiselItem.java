f56d4cb9b6f7d56f10ad00b567d344b2859215c9
Gabriel Custodio <gabrieldsc24@gmail.com>
2025-09-27 21:37:27 -0300

    feat: added old enchanted golden apple recipe and tooltip for the chisel item

--- src/main/java/net/gabecust/tutorialmod/item/custom/ChiselItem.java
+++ src/main/java/net/gabecust/tutorialmod/item/custom/ChiselItem.java
@@ -1,6 +1,8 @@
 package net.gabecust.tutorialmod.item.custom;
 
 import net.gabecust.tutorialmod.block.ModBlocks;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
@@ -8,11 +10,14 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
+import java.util.List;
 import java.util.Map;
 
 public class ChiselItem extends Item {
@@ -51,5 +56,12 @@ public class ChiselItem extends Item {
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+        pTooltipComponents.add(Component.translatable("tooltip.gabecustmod.chisel"));
+
+        super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
     }
 }
